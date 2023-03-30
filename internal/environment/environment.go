d33258623b835af96b4c2c0531f3fa6bae24782e
Andrew Marine <andrew.marine@clever.com>
2023-03-30 16:19:25 -0700

    add env variable utilities

---
+++ internal/environment/environment.go
@@ -0,0 +1,26 @@
+package environment
+
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	ECRAccountID       = envMustString("ECR_ACCOUNT_ID")
+	ShortSHA1          = envMustString("CIRCLE_SHA1")[:7]
+	ECRAccessKeyID     = envMustString("ECR_PUSH_ID")
+	ECRSecretAccessKey = envMustString("ECR_PUSH_SECRET")
+
+	AWSRegions = []string{"us-west-1", "us-west-2", "us-east-1", "us-east-2"}
+
+	Local = os.Getenv("LOCAL") == "true"
+)
+
+func envMustString(key string) string {
+	v := os.Getenv(key)
+	if v == "" {
+		fmt.Println("env variable missing:", key)
+		os.Exit(1)
+	}
+	return v
+}
