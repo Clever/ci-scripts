eb7480caa702623c8b8571e3d3f9cd0ebde38688
Andrew Marine <andrew.marine@clever.com>
2023-03-31 13:11:59 -0700

    make some env variables not required for local

--- internal/environment/environment.go
+++ internal/environment/environment.go
@@ -6,21 +6,21 @@ import (
 )
 
 var (
-	ECRAccountID         = envMustString("ECR_ACCOUNT_ID")
-	ShortSHA1            = envMustString("CIRCLE_SHA1")[:7]
-	ECRAccessKeyID       = envMustString("ECR_PUSH_ID")
-	ECRSecretAccessKey   = envMustString("ECR_PUSH_SECRET")
-	LambdaArtifactBucket = envMustString("LAMBDA_AWS_BUCKET")
-	CatapultURL          = envMustString("CATAPULT_URL")
+	ECRAccountID         = envMustString("ECR_ACCOUNT_ID", true)
+	ShortSHA1            = envMustString("CIRCLE_SHA1", true)[:7]
+	ECRAccessKeyID       = envMustString("ECR_PUSH_ID", false)
+	ECRSecretAccessKey   = envMustString("ECR_PUSH_SECRET", false)
+	LambdaArtifactBucket = envMustString("LAMBDA_AWS_BUCKET", true)
+	CatapultURL          = envMustString("CATAPULT_URL", true)
 
 	AWSRegions = []string{"us-west-1", "us-west-2", "us-east-1", "us-east-2"}
 
 	Local = os.Getenv("LOCAL") == "true"
 )
 
-func envMustString(key string) string {
+func envMustString(key string, localRequired bool) string {
 	v := os.Getenv(key)
-	if v == "" {
+	if v == "" && localRequired {
 		fmt.Println("env variable missing:", key)
 		os.Exit(1)
 	}
