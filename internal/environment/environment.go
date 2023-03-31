d55a086e59d2a07a468596035f581bfc94dcfa47
Andrew Marine <andrew.marine@clever.com>
2023-03-31 11:08:54 -0700

    add some additional environment variables

--- internal/environment/environment.go
+++ internal/environment/environment.go
@@ -6,10 +6,12 @@ import (
 )
 
 var (
-	ECRAccountID       = envMustString("ECR_ACCOUNT_ID")
-	ShortSHA1          = envMustString("CIRCLE_SHA1")[:7]
-	ECRAccessKeyID     = envMustString("ECR_PUSH_ID")
-	ECRSecretAccessKey = envMustString("ECR_PUSH_SECRET")
+	ECRAccountID         = envMustString("ECR_ACCOUNT_ID")
+	ShortSHA1            = envMustString("CIRCLE_SHA1")[:7]
+	ECRAccessKeyID       = envMustString("ECR_PUSH_ID")
+	ECRSecretAccessKey   = envMustString("ECR_PUSH_SECRET")
+	LambdaArtifactBucket = envMustString("LAMBDA_AWS_BUCKET")
+	CatapultURL          = envMustString("CATAPULT_URL")
 
 	AWSRegions = []string{"us-west-1", "us-west-2", "us-east-1", "us-east-2"}
 
