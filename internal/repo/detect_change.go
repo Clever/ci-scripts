b8e266579b51566000859abb07aaceaec254e1a2
Andrew Marine <andrew.marine@clever.com>
2025-01-07 22:29:40 -0700

    add a function for diff checking artifact dependencies

---
+++ internal/repo/detect_change.go
@@ -0,0 +1,63 @@
+package repo
+
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/Clever/catapult/gen-go/models"
+)
+
+// go list -f '{{.Dir}}' -deps ./cmd/arkdb | grep $(pwd) | grep -v '/vendor/'
+
+// DetectArtifactDependencyChange checks if the artifact dependency
+// globs defined in the launch config have changed by using git diff for
+// only the specified file globs. The dependencies are always checked
+// against the base branch (HEAD). More advanced dependency checking is
+// hard and involves persisted caching of some sort which should be
+// left to a build system later on.
+func DetectArtifactDependencyChange(lc *models.LaunchConfig) (bool, error) {
+	if lc.Build.Artifact.Dependencies == nil {
+		return true, nil
+	}
+
+	// Define the find command to search for multiple globs
+	globs := []string{"."}
+	for i, glob := range lc.Build.Artifact.Dependencies {
+		if i == 0 {
+			globs = append(globs, "-name", glob)
+		} else {
+			globs = append(globs, "-o", "-name", glob)
+		}
+	}
+	findCmd := exec.Command("find", globs...)
+	fmt.Println(findCmd.String())
+
+	// Capture the output from find
+	output, err := findCmd.Output()
+	if err != nil {
+		return false, fmt.Errorf("find: %v", err)
+	}
+
+	// Split the output into file paths
+	files := strings.Fields(string(output))
+	if len(files) == 0 {
+		return false, errors.New("no matching files")
+	}
+	fmt.Println(files)
+
+	// Prepare git diff command with the found files
+	args := append([]string{"diff", "--name-only", "HEAD", "--"}, files...)
+	gitCmd := exec.Command("git", args...)
+	fmt.Println(gitCmd.String())
+
+	// Capture the output from git diff
+	output, err = gitCmd.Output()
+	if err != nil {
+		return false, fmt.Errorf("git diff: %v", err)
+	}
+	fmt.Println(string(output))
+
+	return string(output) != "", nil
+}
