adf0e26c9b15df807a3d8dc9776eca9ee622d261
Andrew Marine <andrew.marine@clever.com>
2025-01-08 20:13:42 -0700

    simplify the detect script to only use git

--- internal/repo/detect_change.go
+++ internal/repo/detect_change.go
@@ -1,10 +1,8 @@
 package repo
 
 import (
-	"errors"
 	"fmt"
 	"os/exec"
-	"strings"
 
 	"github.com/Clever/catapult/gen-go/models"
 )
@@ -22,49 +20,11 @@ func DetectArtifactDependencyChange(lc *models.LaunchConfig) (bool, error) {
 		return true, nil
 	}
 
-	// Define the find command to search for multiple globs
-	globs := []string{".", "-type", "f", "("}
-	nots := []string{}
-	for _, glob := range lc.Build.Artifact.Dependencies {
-		if strings.HasPrefix(glob, "!") {
-			nots = append(nots, "-not", "-path", glob[1:])
-			continue
-		}
-
-		if len(globs) == 4 {
-			globs = append(globs, "-path", glob)
-		} else {
-			globs = append(globs, "-o", "-path", glob)
-		}
-	}
-	globs = append(globs, ")")
-	globs = append(globs, nots...)
-	findCmd := exec.Command("find", globs...)
-	fmt.Println(findCmd.String())
-
-	// Capture the output from find
-	output, err := findCmd.Output()
-	if err != nil {
-		return false, fmt.Errorf("find: %v", err)
-	}
-
-	// Split the output into file paths
-	files := strings.Fields(string(output))
-	if len(files) == 0 {
-		return false, errors.New("no matching files")
-	}
-	// You can uncomment the following line to see the files found by
-	// the find command but it is very verbose
-	fmt.Println(files)
-
-	// Prepare git diff command with the found files
-	args := append([]string{"diff", "--name-only", "HEAD", "master", "--"}, files...)
+	args := append([]string{"diff", "--name-only", "HEAD", "master", "--"}, lc.Build.Artifact.Dependencies...)
 	gitCmd := exec.Command("git", args...)
-	// This command is also very verbose
-	// fmt.Println(gitCmd.String())
+	fmt.Println(gitCmd.String())
 
-	// Capture the output from git diff
-	output, err = gitCmd.Output()
+	output, err := gitCmd.Output()
 	if err != nil {
 		return false, fmt.Errorf("git diff: %v", err)
 	}
