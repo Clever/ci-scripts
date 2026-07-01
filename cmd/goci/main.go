750d7b9fc9fd696848e5defd500962cdcbc144a0
Diana Martschenko <diana.martschenko@clever.com>
2026-07-01 14:02:23 -0700

    fix typo

--- cmd/goci/main.go
+++ cmd/goci/main.go
@@ -145,7 +145,7 @@ func run(mode string) error {
 	catapultArtifacts := make([]*catapult.Artifact, 0, len(artifacts))
 	catapultAppIDs := make([]string, 0, len(artifacts))
 	for _, art := range artifacts {
-		if ac, ok := apps[art.ID]; ok && !ac.HasLaunchConfig {
+		if ac, ok := apps[art.ID]; ok && ac.HasLaunchConfig {
 			catapultArtifacts = append(catapultArtifacts, art)
 			catapultAppIDs = append(catapultAppIDs, art.ID)
 		}
